ca27bc7638aa9fa0f93001d965ab6efd123a499f
RickPijnenburg <32143619+RickPijnenburg@users.noreply.github.com>
2021-09-20 13:07:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const makePizza = function(){
+    console.log('Lay down the bottom');
+    console.log('Put tomato sauce on the bottom');
+    console.log('Put salami on the pizza');
+    console.log('Put ananas on the pizza');
+    console.log('Put the pizza in the oven');
+};
+
+const makeSushi = function(){
+    console.log('Lay down green blade');
+    console.log('Put some rice on it');
+    console.log('Put some salamon on it');
+    console.log('Roll the blade');
+    console.log('Cut the roll in pieces');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
